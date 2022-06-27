899b0042d04ca7244bd64cb418e4d3bd654f4132
mkleeboy3 <mkleeboy3@huray.net>
2022-06-27 15:57:45 +0900

    [UPDATE] API Key 변수를 Placeholder로 변경

---
+++ UnsplashViewer/Utilities/Constants/HTTPConstants.swift
@@ -0,0 +1,14 @@
+//
+//  HTTPConstants.swift
+//  UnsplashViewer
+//
+//  Created by Ming on 2022/06/21.
+//
+
+import Foundation
+
+class HTTPConstants {
+  public static let accessToken = "<#Your API Key#>"
+  
+  public static let baseURL = "https://api.unsplash.com/"
+}
